3d6dbfb60946ef83d862447fe27d372dbde299dc
alugocp <alugocp@aim.com>
2020-09-14 17:03:05 -0400

    Switched to absolulute paths

--- components/head.php
+++ components/head.php
@@ -1,5 +1,5 @@
 <meta charset="utf-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="css/style.css"/>
-<script src="js/jquery-3.5.1.min.js"></script>
-<script src="js/events.js"></script>
+<link rel="stylesheet" href="/css/style.css"/>
+<script src="/js/jquery-3.5.1.min.js"></script>
+<script src="/js/events.js"></script>
