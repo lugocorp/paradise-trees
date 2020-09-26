88fafa9974e96ca70e1805dca1d887b10840adb6
alugocp <alugocp@aim.com>
2020-09-25 22:27:41 -0400

    Restructured as a WordPress theme

--- index.php
+++ index.php
@@ -1,97 +1,17 @@
-<html>
-<head>
-  <title>Paradise Trees</title>
-  <?php $DIR=".";?>
-  <?php include("components/head.php");?>
-</head>
-<body>
-  <div class="container">
+<?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
 
-    <!-- Splash -->
-    <div class="splash" style="background-image:url('<?php echo($DIR);?>/images/madagascar1.jpg')">
-      <div class="fade-in">
-        <img src="<?php echo($DIR);?>/images/paradise-icon.jpg"/>
-        <h1>PARADISE TREES</h1>
-        <p>
-          Clean Your Environmental Impact by Supporting Green Projects in Madagascar
-        </p>
-      </div>
-    </div>
-    <div class="mobile-splash" style="background-image:url('<?php echo($DIR);?>/images/madagascar1.jpg')">
-      <div class="fade-in">
-        <img src="<?php echo($DIR);?>/images/paradise-icon.jpg"/>
-      </div>
-      <div class="fade-in">
-        <h1>PARADISE TREES</h1>
-        <p>
-          Clean Your Environmental Impact by Supporting Green Projects in Madagascar
-        </p>
-      </div>
-    </div>
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
 
-    <!-- Sections -->
-    <div class="section thin">
-      <p>
-        At Paradise Trees we believe addressing your environmental impact should be simple. That's why we have created this platform which provides direct access to emissions reducing projects throughout Madagascar. As an individual or company, you can follow our simple two-step process which allows you to; 1) Quantify your environmental impact in terms of a carbon footprint and 2) Offset that footprint through tree planting or fire protecting projects.
-      </p>
-      <p>
-        Addressing your environmental impact is easy and creates benefits for everyone involved. To learn more about our two-step process as well as more information on carbon, select one of the options below.
-      </p>
-      <div class="buttons">
-        <div>
-          <a href="individuals"><div class="offset">For Individuals</div></a>
-        </div>
-        <div>
-          <a href="companies"><div class="offset">For Companies</div></a>
-        </div>
-      </div>
-    </div>
-    <div class="green img-left">
-      <div class="image" style="background-image:url('<?php echo($DIR);?>/images/madagascar2.jpg')"></div>
-      <div>
-        <h1>Why Madagascar?</h1>
-        <p>
-          Madagascar is one of the most unique places on earth due to its isolation as an island on the east coast of Africa. It is home to more than 200,000 animal, plant and insect species, nearly 80 percent of them being endemic; meaning they exit nowhere else. Aside from its rich biodiversity, the people throughout the land are mainly villagers with preserved cultures. To visit Madagascar is like coming to a different planet! And when we visited, we were grateful to have created relationships with some of the most down-to-earth projects in Madagascar largely run by the people of Madagascar! Their goals are grounded in environmental protection, reforestation and community development projects over 706 square kilometers of land. Being introduced to such a large opportunity to contribute to positive change brought life to our platform, which was created to allow anyone from anywhere in the world to directly get involved.
-        </p>
-      </div>
-    </div>
-    <div class="section thin">
-      <h1>Your Impact</h1>
-      <p>
-        Each tree you plant and even the slightest support towards the 'Rainbow' project creates more than just a positive environmental impact and cleaning of your carbon footprint. Any support significantly increases job opportunities for local villagers which leads to an improvement of their overall community. They learn how to plant, protect trees, and even get involved in our sustainable agriculture education; All of which directly improve their communities and future. That is why your impact reaches all throughout Madagascar!
-      </p>
-      <div class="buttons">
-        <div><a class="offset" href="donate#trees">Buy a Tree</a></div>
-        <div><a class="offset" href="donate#rainbow">Support Rainbow</a></div>
-      </div>
-    </div>
-    <div class="section green">
-      <h1>Gallery</h1>
-    </div>
-    <div class="section contact">
-      <h1 class="center">Contact</h1>
-      <div class="contact-grid">
-        <div>
-          <a class="contact-info" href="mailto:1borna@gmail.com">
-            <img src="images/mail.svg"/>
-            <p>Email</p>
-          </a>
-        </div>
-        <div>
-          <a class="contact-info" href="https://www.instagram.com/paradise.trees/">
-            <img src="images/instagram.svg"/>
-            <p>Instagram</p>
-          </a>
-        </div>
-        <div>
-          <a class="contact-info" href="http://lugocorp.net">
-            <img src="images/facebook.svg"/>
-            <p>Facebook</p>
-          </a>
-        </div>
-      </div>
-    </div>
-    <?php include("components/footer.php");?>
-  </div>
-</body>
-</html>
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
